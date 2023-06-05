f73860f43be71f8fda78015df215b1676092ef8c
CHRISKINUNGI <chrskinungi@gmail.com>
2023-06-06 01:45:12 +0300

    commit to add 102-magic_calculation.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -3,5 +3,5 @@ import sys
 
 message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
 
-sys.stderr.write(f"{message}\n")
+sys.stderr.write(f"{message:s}\n")
 sys.exit(1)
---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    constant = 98
+    result = constant + (a ** b)
+    return result
