6112248967983035376d8e58d8e4be9381e799c2
CHRISKINUNGI <chrskinungi@gmail.com>
2023-06-06 01:16:59 +0300

    commit to add  7-edges.py file

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"First 3 letters: {word[:3]:s}")
+print(f"Last 2 letters: {word[-2:]:s}")
+print(f"Middle word: {word[1:8]:s}")
