ec8fef0702dce6b076223fb5b601bb6d666d12cd
CHRISKINUNGI <chrskinungi@gmail.com>
2023-06-07 02:51:56 +0300

    commit to add 100-print_tebahpla.py file

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,10 @@
 def print_alphabet_reverse():
+    """ the ord function converts the ASCII value
+    back to its corresponding character. """
+
     for i in range(ord('Z'), ord('A') - 1, -1):
+
         print("{}{}".format(chr(i + 32) if i % 2 == 1 else chr(i), ""), end="")
 
-print_alphabet_reverse()
 
+print_alphabet_reverse()
