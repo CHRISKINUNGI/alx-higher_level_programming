64f433b0281e033a591069d00b65b8deb0014b46
CHRISKINUNGI <chrskinungi@gmail.com>
2023-06-12 22:49:03 +0300

    commit to add 4-new_in_list.py  file

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
+    my_list1 = my_list[:]
     if idx < 0:
-        return my_list
+        return my_list1
     elif idx >= len(my_list):
-        return my_list
+        return my_list1
     else:
-        my_list[idx] = element
-        return my_list
+        my_list1[idx] = element
+        return my_list1
