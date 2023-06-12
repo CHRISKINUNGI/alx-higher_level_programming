4b32b8726d1c9a15a48957937ed06e52f8e1c9bc
CHRISKINUNGI <chrskinungi@gmail.com>
2023-06-12 21:01:39 +0300

    commit to correct 2-replace_in_list.py  file

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    elif idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        return my_list[idx] = element
+        my_list[idx] = element
+        return my_list
