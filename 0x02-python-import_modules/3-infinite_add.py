f16d8e0e7f4a1d43b2d872644145918c1c543ccd
CHRISKINUNGI <chrskinungi@gmail.com>
2023-06-14 13:15:10 +0300

    commit to add 3-infinite_add.py file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
- if _name_ == "_main_":
-     sum = 0
-     for i in range(len(sys.argv) - 1):
-         sum += int(sys.argv[i + 1])
-     print("{}".format(sum))
+
+if __name__ == "__main__":
+    sum = 0
+    for i in range(len(sys.argv) - 1):
+        sum += int(sys.argv[i+1])
+    print("{}".format(sum))
