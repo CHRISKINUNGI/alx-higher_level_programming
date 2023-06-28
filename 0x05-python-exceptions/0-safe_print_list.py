c5889b4b39527f2cf312dbce9c7c507995cfee57
CHRISKINUNGI <chrskinungi@gmail.com>
2023-06-28 12:18:59 +0300

    commit to add 0-safe_print_list.py file correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     index = 0
     for count in range(x):
         try:
-            print("{0}".format(my_list[count]), end="")
+            print("{}".format(my_list[count]), end="")
             index += 1
         except IndexError:
             break
