4888c267b26e3a844979ff9907dd754aeb8318c2
CHRISKINUNGI <chrskinungi@gmail.com>
2023-07-01 01:12:47 +0300

    commit to add 2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+def safe_print_list_integers(my_list=[], x=0):
+    for count in my_list:
+        while count < x:
+            try:
+                if my_list[count] is not int:
+                    pass
+                else:
+                    print("{:d}".format(my_list(count), end=""))
+                    count += 1
+            except IndexError:
+                pass
+    print()
+    return count
