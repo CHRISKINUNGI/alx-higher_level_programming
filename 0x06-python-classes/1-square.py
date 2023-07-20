152e059ea803139c3b50960d0973372338dbdcb9
CHRISKINUNGI <chrskinungi@gmail.com>
2023-07-20 12:31:02 +0300

    commit to add 1-square.py  file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,6 +5,7 @@
 
 class Square:
 
-    """ Class defination """
-    def __init__(self, size):
+    """ Class defination"""
+    def __init__(self, size)i:
+        """ class initialization """
         self.size = size
