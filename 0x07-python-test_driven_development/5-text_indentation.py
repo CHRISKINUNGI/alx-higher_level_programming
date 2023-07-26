80964fc4bd7c7f47a25049f97030c3bd999f3b85
CHRISKINUNGI <chrskinungi@gmail.com>
2023-07-26 20:57:29 +0300

    commit to add 5-text_indentation.py, tests/5-text_indentation.txt

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" module containing function  """
+
+
+def text_indentation(text):
+    """ 
+        text_indentation(text): function to prints a 
+        text with 2 new lines after each of these characters: ., ? and :
+
+        args:
+            text (string): string to 
+        raise: 
+            TypeError(text must be a string)
+
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    i = 0
+    while i < len(text):
+        if text[i] == "." or text[i] == "?" or text[i] == ":":
+            print(text[i])
+            print()
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+        else:
+            print(text[i], end="")
+            i += 1
