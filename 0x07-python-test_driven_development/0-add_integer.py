892349de6f2950b3cc613d7a99b12cb3bfcc3e54
CHRISKINUNGI <chrskinungi@gmail.com>
2023-07-26 17:37:51 +0300

    commit to add 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -20,6 +20,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return int(a) + int(b)
+    result = int(a) + int(b)
+    return int(result)
