cc26c761a16de6f421e42491f435744fbabe86a4
CHRISKINUNGI <chrskinungi@gmail.com>
2023-07-30 01:51:42 +0300

    commit to correct 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+""" Module that contains a function that returns a list with
+the available attributes and methods of an object """
 
-""" Module that contains a function that returns a list with the available attributes and methods of an object """
 
 def lookup(obj):
-    """ Function that returns a list with the available attributes and methods of an object """
+    """ Function that returns a list with the available
+    attributes and methods of an object """
     return dir(obj)
