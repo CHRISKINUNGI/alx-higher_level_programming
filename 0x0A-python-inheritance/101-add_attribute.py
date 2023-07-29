74ed592b76b0c4912e2e4d80e6291aa3724f8e73
CHRISKINUNGI <chrskinungi@gmail.com>
2023-07-30 02:21:50 +0300

    commit to add 4-inherits_from.py file

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 
-def add_attribute(obj, name, value):
-    """ Adds a new attribute to an object if it’s possible """
-    if hasattr(obj, "__dict__"):
-        setattr(obj, name, value)
-    else:
+def add_attribute(obj, attr_name, attr_value):
+    """Add a new attribute to an object if possible.
+
+    Args:
+        obj (object): The object to which the attribute will be added.
+        attr_name (str): The name of the new attribute.
+        attr_value (any): The value of the new attribute.
+
+    Raises:
+        TypeError: If the object cannot have new attributes.
+
+    Returns:
+        None
+    """
+    if not hasattr(obj, '__dict__'):
         raise TypeError("can't add new attribute")
+    setattr(obj, attr_name, attr_value)
