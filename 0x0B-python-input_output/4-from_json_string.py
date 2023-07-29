8bd3284bda35ea6eeb2f6df57fbe9124bf0ead47
CHRISKINUNGI <chrskinungi@gmail.com>
2023-07-30 02:43:35 +0300

    commit to add formarting

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
 """From JSON string to Object module"""
 
 import json
 
+
 def from_json_string(my_str):
-    """Returns an object (Python data structure) represented by a JSON string"""
+    """Returns an object (Python data structure)
+    represented by a JSON string"""
     return json.loads(my_str)
