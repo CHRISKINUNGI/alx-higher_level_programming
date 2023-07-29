2a68f959c11e4f980c1ca6f061457ddaefa2d807
CHRISKINUNGI <chrskinungi@gmail.com>
2023-07-30 02:37:59 +0300

    commit to add formarting

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
 """Write file module"""
 
+
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written"""
+    """Writes a string to a text file (UTF8) and returns the
+    number of characters written"""
     with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
