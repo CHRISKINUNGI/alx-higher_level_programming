f2caefa5029a41104b4256d6b75d5a03a1b336a2
CHRISKINUNGI <chrskinungi@gmail.com>
2023-08-02 14:42:07 +0300

    commit to add corrections to inheritance repo

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
+"""Module that checks if an object is an instance of a class"""
+
 
 def inherits_from(obj, a_class):
+    """Function that returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class;
+    otherwise False.
+    Args:
+        obj: object to check
+        a_class: class to check
+    Returns:
+        True if the object is an instance of a class that inherited
+        (directly or indirectly) from the specified class;
+        otherwise False.
     """
-    Check if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
 
-    :param obj: The object to check.
-    :param a_class: The class to check against.
-    :return: True if the object is an instance of a subclass of a_class, otherwise False.
-    """
-    # Check if obj is an instance of a_class or any of its subclasses
-    is_subclass = isinstance(obj, a_class)
-
-    # Check if obj is not an instance of a_class itself but a subclass
-    is_subclass_but_not_base = type(obj) != a_class
-
-    # Return True if obj is a subclass of a_class, otherwise False
-    return is_subclass and is_subclass_but_not_base
+    return isinstance(obj, a_class) and type(obj) != a_class
