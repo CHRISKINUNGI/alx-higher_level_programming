acb0e1b1c3a1a9468e5e001d983ac08f8e901339
CHRISKINUNGI <chrskinungi@gmail.com>
2023-09-22 22:49:46 +0300

    commit to add 12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    script that changes the name of a State object from the database
+    hbtn_0e_6_usa
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """
+        args:
+            mysql username -argv[1]
+            mysql password -argv[2]
+            database name -argv[3]
+    """
+
+    db_connection = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_connection)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = "New Mexico"
+    session.commit()
+
+    session.close()
