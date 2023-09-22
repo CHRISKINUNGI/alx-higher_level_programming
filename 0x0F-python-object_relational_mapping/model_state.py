3694a8f1f9366fe81aa513b4208a450d335fb83f
CHRISKINUNGI <chrskinungi@gmail.com>
2023-09-22 12:06:02 +0300

    commit to add model_states.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,14 +4,34 @@
     State and an instance Base = declarative_base()
 """
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData
+
+""" creating engine """
+engine = create_engine("mysql://root:Kitchen2020.@localhost:3306")
+
+""" creating Session """
+Session = sessionmaker(bind=engine)
+session = Session()
+
+""" Base  declaration """
+Base = declarative_base()
 
 
 class State(Base):
     """
-        args
+        args:
+             id
+             name
     """
 
-    __tablename__ =  'states'
+    __tablename__ = 'states'
 
-    id=column(Integer, primary_key=True, )
-    name=column(String(128), Null=False) 
+    id = column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = column(String(128), nullable=False)
+
+
+""" Migration of the data """
+Base.metadata.create_all(engine)
