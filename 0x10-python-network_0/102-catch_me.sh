aff7ff1cb1ee2eb5f7c797818b29d2f4d145328a
CHRISKINUNGI <chrskinungi@gmail.com>
2023-09-29 16:49:50 +0300

    commit to add 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #Bash script that makes a request to 0.0.0.0:5000/catch_me
-curl -sL -X PUT -d "user_id=98" "0.0.0.0:5000/catch_me"
+curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
+
