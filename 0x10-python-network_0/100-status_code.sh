7d1e769c1cf7b4e2bb14eaef338d9d322fb49226
CHRISKINUNGI <chrskinungi@gmail.com>
2023-09-29 16:07:53 +0300

    commit to add 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Send a GET request to the URL passed as an argument and display the status code
-curl -s -o /dev/null -w "%{http_code}" "$1"
+echo "(curl -s -o /dev/null -w "%{http_code}" "$1")
