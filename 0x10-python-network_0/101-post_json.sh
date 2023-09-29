e68e7cc1eccbd15f769f23a7e0335c240429e06a
CHRISKINUNGI <chrskinungi@gmail.com>
2023-09-29 16:27:07 +0300

    commit to add 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Send a POST request with the JSON data from the file and display the response body
-curl -sI -H "Content-Type: application/json" -X POST --data @"$2" "$1"
+jq empty "$2" && curl -s -H "Content-Type: application/json" -X POST --data @"$2" "$1"
 
