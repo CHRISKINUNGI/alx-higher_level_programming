40864ec6846c1498a6568b4b89ee130b8d78ab9c
CHRISKINUNGI <chrskinungi@gmail.com>
2023-11-14 00:56:34 +0300

    commit to add 5-to_integer.js file

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const process = require('process');
+const myNumber = parseInt(process.argv[2]);
+
+if (isNaN(myNumber)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + myNumber);
+}
