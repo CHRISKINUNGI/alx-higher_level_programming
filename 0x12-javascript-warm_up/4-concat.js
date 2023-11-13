98da1a3cc1722b4cfbfa1678b9f0b093a9ea5e5b
CHRISKINUNGI <chrskinungi@gmail.com>
2023-11-14 00:32:16 +0300

    commit to add correction to 4-concat.js file

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 const process = require('process');
-if (process.argv.length <= 2) {
-  console.log('no args');
-} else {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
