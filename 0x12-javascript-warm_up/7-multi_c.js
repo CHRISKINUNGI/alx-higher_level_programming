ab330cc19aaa20423ca9d4449f5bca8e98ad49b4
CHRISKINUNGI <chrskinungi@gmail.com>
2023-11-14 01:53:13 +0300

    commit to addcorrection to 7-multi_c.js file

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const process = require('process');
 const times = parseInt(process.argv[2]);
+
 if (isNaN(times)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < process.argv[2]; i++) {
-    console.log('Cis fun');
+    console.log('C is fun');
   }
 }
