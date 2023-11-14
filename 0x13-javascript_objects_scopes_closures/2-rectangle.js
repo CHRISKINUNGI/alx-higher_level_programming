9e7b5b4520eed3f5a910348a64aa5b0b0eb885d5
CHRISKINUNGI <chrskinungi@gmail.com>
2023-11-15 00:59:06 +0300

    commit to add 2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (this.isValidDimensions(w, h)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return Rectangle {};
+    }
+  }
+
+  isValidDimensions (width, height) {
+    return width > 0 && height > 0 && Number.isInteger(width) && Number.isInteger(height);
+  }
+}
+module.exports = Rectangle;
