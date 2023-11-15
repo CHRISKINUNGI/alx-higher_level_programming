31bad2a230424c0d3dcdd181ad961e86f2841673
CHRISKINUNGI <chrskinungi@gmail.com>
2023-11-15 03:50:39 +0300

    commit to add 100-map.js

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const data = require('./100-data');
+const initialList = data.list;
+
+const newList = initialList.map((value, index) => value * index);
+
+console.log(initialList);
+console.log(newList);
