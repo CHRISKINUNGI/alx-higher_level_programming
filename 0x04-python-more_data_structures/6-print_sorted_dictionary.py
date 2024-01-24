5160d282b426a9c453199dfc962f46a07cfef8e6
CHRISKINUNGI <chrskinungi@gmail.com>
2024-01-24 13:50:28 +0300

    commit to add 7-update_dictionary.py file

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    dict_keys = sorted(a_dictionary.keys())
-    for key in dict_keys:
-        value = a_dictionary[key]
-        print(f'{key}: {value}')
+    if a_dictionary is not None:
+        dict_keys = sorted(a_dictionary.keys())
+        for key in dict_keys:
+            value = a_dictionary[key]
+            print(f'{key}: {value}')
